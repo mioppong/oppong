ac5cb831b085e6e8431fc90a40ec9c2cc373856f
oppong <michaeloppong123@gmail.com>
2021-01-02 00:17:24 -0500

    still workin

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,36 +1,69 @@
 import React, { Component } from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
+import { Layout, Menu, Breadcrumb, Button } from "antd";
 import "./index.css";
+import logo from "../assets/cyborg.png";
+import { Anchor } from "antd";
+const { Link } = Anchor;
 const { Header, Content, Footer } = Layout;
+
 export default class Counter extends Component {
   render() {
     return (
       <Layout>
-        <Header
-          style={{
-            position: "fixed",
-            zIndex: 1,
-            width: "100%",
-            background: "#fff",
-          }}
-        >
-          <div className="logo" />
-          <Menu
-            theme="light"
-            mode="horizontal"
-            defaultSelectedKeys={["2"]}
-            style={{ lineHeight: "64px" }}
-          >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
+        <Header className="header">
+          <img src={logo} style={{ height: "100px" }} />
+          <div className="name-title">Michael Oppong</div>
+
+          <div className="menu-container">
+            <Anchor affix={false}>
+              <Link className="menu-text" href="#cool" title="Mobile" />
+            </Anchor>
+
+            <Anchor affix={false}>
+              <Link className="menu-text" href="#cool" title="IOT" />
+            </Anchor>
+
+            <Anchor affix={false}>
+              <Link className="menu-text" href="#cool" title="Analytics" />
+            </Anchor>
+          </div>
         </Header>
+
+        <Content className="about-container" style={{ marginTop: "100px" }}>
+          <div className="project-title">About me</div>
+          <div
+            style={{ background: "#fff", padding: 24, minHeight: 380 }}
+          ></div>
+        </Content>
+
+        <div className="new-project-section" id="mobile">
+          MOBILE
+        </div>
+        <Content className="project-container" style={{ marginTop: "100px" }}>
+          <div className="project-title"></div>
+          <div
+            style={{ background: "#fff", padding: 24, minHeight: 380 }}
+          ></div>
+        </Content>
+
         <Content style={{ padding: "0 50px", marginTop: 64 }}>
           <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              background: "#fff",
+              borderRadius: 20,
+              padding: 24,
+              minHeight: 380,
+            }}
+          >
+            Content
+          </div>
+        </Content>
+        <Content style={{ padding: "0 50px", marginTop: 64 }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
           </Breadcrumb>
           <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
             Content
@@ -39,8 +72,6 @@ export default class Counter extends Component {
         <Content style={{ padding: "0 50px", marginTop: 64 }}>
           <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
           <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
             Content
@@ -49,15 +80,16 @@ export default class Counter extends Component {
         <Content style={{ padding: "0 50px", marginTop: 64 }}>
           <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
-          <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
-            Content
+          <div
+            id="michael"
+            style={{ background: "#fff", padding: 24, minHeight: 380 }}
+          >
+            Michael Content
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
+        <Footer style={{ textAlign: "center" }} id="cool">
+          Created by Michael Oppong
         </Footer>
       </Layout>
     );
